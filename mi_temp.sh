082fea60d05dbcf03af6b8e768ccd7d65d3e11f1
Martin <bircoe@gmail.com>
2019-02-08 10:17:30 +0000

    Script updated for multiple sensors

--- mi_temp.sh
+++ mi_temp.sh
@@ -1,49 +1,72 @@
 #!/bin/bash
 
-bt="4C:65:A8:DB:D1:32"
-sensor="mi_temp"
-room="Lounge_Room"
-ip="192.168.1.60"
+mqtt_topic="mi_temp"
+mqtt_ip="192.168.1.60"
 
+sensors_file="sensors"
 cel=$'\xe2\x84\x83'
 per="%"
 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 echo "Enabling LE Mode"
-btmgmt le on
+sudo btmgmt le on
 
-RET=1
-until [ ${RET} -eq 0 ]; do
-    echo "Getting Temperature and Humidity..."
-    data=$(/usr/bin/timeout 20 /usr/bin/gatttool -b $bt --char-write-req --handle=0x10 -n 0100 --listen | grep "Notification handle" -m 2)
-    RET=$?
-    sleep 5
-done
+while read -r item; do
+    sensor=(${item//,/ })
+    mac="${sensor[0]}"
+    name="${sensor[1]}"
+    echo -e "\n${YELLOW}Sensor: $name ($mac)${NC}"
 
-RET=1
-until [ ${RET} -eq 0 ]; do
-    echo "Getting Battery Level..."
-    battery=$(/usr/bin/gatttool -b $bt --char-read --handle=0x18 | cut -c 34-35)
-    RET=$?
-    sleep 5
-done
+    RET=1
+    until [ ${RET} -eq 0 ]; do
+        echo -n "  Getting $name Temperature and Humidity... "
+        data=$(/usr/bin/timeout 20 /usr/bin/gatttool -b $mac --char-write-req --handle=0x10 -n 0100 --listen 2>&1 | grep "Notification handle" -m 2)
+        RET=$?
+        if [ ${RET} -ne 0 ]; then
+            echo -e "${RED}failed, waiting 5 seconds before trying again${NC}"
+            sleep 5
+        else
+            echo -e "${GREEN}success${NC}"
+        fi
+    done
 
-temp=$(echo $data | tail -1 | cut -c 42-54 | xxd -r -p)
-humid=$(echo $data | tail -1 | cut -c 64-74 | xxd -r -p)
-batt=$(echo "ibase=16; $battery"  | bc)
-echo "Temperature: $temp$cel"
-echo "Humidity: $humid$per"
-echo "Battery Level: $batt$per"
+    RET=1
+    until [ ${RET} -eq 0 ]; do
+        echo -n "  Getting $name Battery Level..."
+        battery=$(/usr/bin/gatttool -b $mac --char-read --handle=0x18 2>&1 | cut -c 34-35)
+        RET=$?
+        if [ ${RET} -ne 0 ]; then
+            echo -e "${RED}failed, waiting 5 seconds before trying again${NC}"
+            sleep 5
+        else
+            echo -e "${GREEN}success${NC}"
+        fi
+    done
 
-echo -e -n "\nPublishing data via MQTT... "
-if [[ "$temp" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-    /usr/bin/mosquitto_pub -h $ip -V mqttv311 -t "/$sensor/$room/temperature" -m "$temp"
-fi
+    temp=$(echo $data | tail -1 | cut -c 42-54 | xxd -r -p)
+    humid=$(echo $data | tail -1 | cut -c 64-74 | xxd -r -p)
+    batt=$(echo "ibase=16; $battery"  | bc)
+    echo "  Temperature: $temp$cel"
+    echo "  Humidity: $humid$per"
+    echo "  Battery Level: $batt$per"
 
-if [[ "$humid" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-    /usr/bin/mosquitto_pub -h $ip -V mqttv311 -t "/$sensor/$room/humidity" -m "$humid"
-fi
+    echo -e -n "  Publishing data via MQTT... "
+    if [[ "$temp" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        /usr/bin/mosquitto_pub -h $mqtt_ip -V mqttv311 -t "/$mqtt_topic/$name/temperature" -m "$temp"
+    fi
 
-if [[ "$batt" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-    /usr/bin/mosquitto_pub -h $ip -V mqttv311 -t "/$sensor/$room/battery" -m "$batt"
-fi
-echo "done"
+    if [[ "$humid" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        /usr/bin/mosquitto_pub -h $mqtt_ip -V mqttv311 -t "/$mqtt_topic/$name/humidity" -m "$humid"
+    fi
+
+    if [[ "$batt" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        /usr/bin/mosquitto_pub -h $mqtt_ip -V mqttv311 -t "/$mqtt_topic/$name/battery" -m "$batt"
+    fi
+    echo -e "done\n"
+done < "$sensors_file"
+
+echo "Finished"
